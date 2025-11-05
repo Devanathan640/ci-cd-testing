8633fc398d528d68f064c93faffe900380bac86c
Devanthan <devanathan640@gmail.com>
2025-11-05 14:41:14 +0530

    testing

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,10 +1,13 @@
 import pytest
-from src.testing import add,sub
+from src.testing import add,sub,mul
 
 def test_add():
     assert add(2,4)==6
 
 def test_sub():
     assert sub(4,2)==2
+
+def test_mul():
+    assert mul(2,3)==6
 
 
